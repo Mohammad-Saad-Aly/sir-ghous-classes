bc85da1cc42f05f824adb89674db339a14cd557b
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-03-20 08:03:37 +0500

    date methods

---
+++ class 4/app.js
@@ -0,0 +1,27 @@
+// var str = "78"
+// var test = parseFloat(str)
+// var test = Number(str)
+// console.log(typeof(test))
+
+// var num = 10
+// var test = num.toString()
+// console.log(typeof(test))
+
+// var num = Math.random() * 3
+// console.log(num.toFixed(0))
+
+// var time = new Date().toString().slice(16,25)
+// console.log(time)
+
+// var time = new Date().getTime()
+// console.log(time)
+
+// var days = ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat']
+// var time = new Date().getDay()
+// console.log(days[time])
+
+// var time = new Date().getTime()
+// var dob = new Date(prompt('Enter your date of birth')).getTime()
+// var age = time - dob
+// var formula = age / (1000 * 60 * 60 * 24 * 365)
+// console.log(`my age is ${Math.floor(formula)}`)
